1afc81c68690bcc478dab4a496a441e3e451160b
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-01-19 23:08:05 +0900

    構造とスタイル作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,37 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了なTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>タスク１</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>タスク2</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了なTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>タスク3</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
