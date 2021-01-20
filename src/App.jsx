a2f7d0f68f73157d539e7208ec6e336f93eba383
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-01-21 07:57:51 +0900

    タスクの削除機能 ・削除ボタンにクリックエベントを割り当てる ・クリックイベントの関数を定義 ・どれが削除されたか判別できるようにindexを引数とする

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,13 @@ export const App = () => {
     setTodoText("");
     // alert(newTodos);
   };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+    alert("削除");
+  };
   return (
     <>
       <div className="input-area">
@@ -38,12 +45,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了なTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
